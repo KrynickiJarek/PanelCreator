ff89ed9cdf7430a06fa3826376fc4406d6f8efc4
KrynickiJarek <krynickijarek@gmail.com>
2021-01-28 11:48:57 +0100

    sass added

---
+++ src/components/CreatorHeader/CreatorHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./_creatorheader.scss"
+
+
+const CreatorHeader = () => {
+    return (
+        <h1>KREATOR PANELI AMPIO</h1>
+    );
+};
+
+export default CreatorHeader;
---
+++ src/layouts/App/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import CreatorHeader from "../../components/CreatorHeader/CreatorHeader"
+
+function App() {
+  return (
+    <div className="App">
+      {/*<header className="App-header" >*/}
+      {/*  <h1>Ampio</h1>*/}
+      {/*</header>*/}
+      <CreatorHeader/>
+    </div>
+  );
+}
+
+export default App;
