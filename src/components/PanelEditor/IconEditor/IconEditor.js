b3ddb0f44cf00c30430a254143c03d241a7cb0c8
Jaroslaw Krynicki <krynickijarek@gmail.com>
2021-03-26 12:42:09 +0100

    webpack added and fixed

--- src/components/PanelEditor/IconEditor/IconEditor.js
+++ src/components/PanelEditor/IconEditor/IconEditor.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Tabs } from 'antd';
+import iconCategories from "./iconCategories"
+
 
 const { TabPane } = Tabs;
 
 const IconEditor = () => {
-    
-const iconCategories = ["Bezpieczeństwo", "Bramy i rolety","Dom","Oświetlenie","Megle","Muzyka","Ogród","Ogrzewanie","Pełne ikony","Pomieszczenia",
-                        "Sceny","Urządzenia","Znaki","Numery","Ulubione","Własne"]
 
+    
+    
 
     return (
       <div>
-        <Tabs defaultActiveKey="1" tabPosition="left" style={{ height: 700}} tabBarGutter={-15} centered={false}>
+        <Tabs defaultActiveKey="1" tabPosition="left" style={{ height: 700}} tabBarGutter={-10} centered={false}>
           {iconCategories.map((el,i) => (
-            <TabPane tab={el} key={i} >
-              Content of tab {el}
+            <TabPane tab={el.name} key={i} >
+              Content of tab {el.name}
             </TabPane>
           ))}
         </Tabs>
@@ -24,68 +25,3 @@ const iconCategories = ["Bezpieczeństwo", "Bramy i rolety","Dom","Oświetlenie"
 
 export default IconEditor;
 
-
-// import React, {useState} from 'react';
-// import { Tabs, Radio } from 'antd';
-
-// const { TabPane } = Tabs;
-
-// const IconEditor = () => {
-//     const [mode, setMode] = useState("top")
-    
-
-//   const handleModeChange = e => {
-//     setMode(e.target.value);
-      
-//   };
-
-//     return (
-//       <div>
-//         <Radio.Group onChange={handleModeChange} value={mode} style={{ marginBottom: 8 }}>
-//           <Radio.Button value="top">Horizontal</Radio.Button>
-//           <Radio.Button value="left">Vertical</Radio.Button>
-//         </Radio.Group>
-//         <Tabs defaultActiveKey="1" tabPosition={mode} style={{ height: 220 }}>
-//           {[...Array.from({ length: 30 }, (v, i) => i)].map(i => (
-//             <TabPane tab={`Tab-${i}`} key={i} disabled={i === 28}>
-//               Content of tab {i}
-//             </TabPane>
-//           ))}
-//         </Tabs>
-//       </div>
-//     );
-//   }
-
-// export default IconEditor;
-
-
-
-
-// import React from 'react';
-// import { Tabs, Radio } from 'antd';
-
-
-// const IconEditor = () => {
-
-//     const { TabPane } = Tabs;
-//     function callback(key) {
-//       console.log(key);
-//     }
-
-
-//     return (
-//         <>
-//             <h1>IconEditor</h1>
-
-//             <Tabs defaultActiveKey="1" tabPosition={mode} style={{ height: 220 }}>
-//           {[...Array.from({ length: 30 }, (v, i) => i)].map(i => (
-//             <TabPane tab={`Tab-${i}`} key={i} disabled={i === 28}>
-//               Content of tab {i}
-//             </TabPane>
-//           ))}
-//         </Tabs>
-//         </>
-//     );
-// };
-
-// export default IconEditor;
---
+++ src/components/PanelEditor/IconEditor/iconCategories.js
@@ -0,0 +1,68 @@
+const iconCategories = [
+    {
+        name: "Bezpieczeństwo", 
+        folder:"Bezpieczeństwo"
+    },
+    {
+        name: "Bramy i rolety",
+        folder: "Bramy i rolety"
+    },
+    {
+        name: "Dom",
+        folder:"Dom"
+    },
+    {
+        name: "Oświetlenie",
+        folder:"Oświetlenie"
+    },
+    {
+        name: "Meble",
+        folder:"Meble"
+    },
+    {
+        name: "Muzyka",
+        folder:"Muzyka"
+    },
+    {
+        name: "Ogród",
+        folder:"Ogród",
+    },
+    {
+        name: "Ogrzewanie",
+        folder:"Ogrzewanie",
+    },
+    {
+        name: "Pełne ikony",
+        folder: "Pełne ikony"
+    },
+    {
+        name: "Pomieszczenia",
+        folder:"Pomieszczenia"
+    },
+    {
+        name: "Sceny",
+        folder:"Sceny",
+    },
+    {
+        name: "Urządzenia",
+        folder:"Urządzenia",
+    },
+    {
+        name: "Znaki",
+        folder:"Znaki",
+    },
+    {
+        name: "Numery",
+        folder:"Numery",
+    },
+    {
+        name: "Ulubione",
+        folder:"Ulubione",
+    },
+    {
+        name: "Własne",
+        folder:"Własne"
+    },
+];
+
+export default iconCategories
