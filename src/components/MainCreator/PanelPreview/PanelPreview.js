22edb08ffc991d01239d2804351dd8b8818c3e6b
Jarosław Krynicki <jaroslawkrynicki@MacBook-Pro-Jarosaw.local>
2021-08-24 10:52:42 +0200

    error fix

--- src/components/MainCreator/PanelPreview/PanelPreview.js
+++ src/components/MainCreator/PanelPreview/PanelPreview.js
@@ -4313,8 +4313,7 @@ const PanelPreview = ({
                                 }
                               </div>
                             )}
-                            {(frame.framePrint.xxx.text !== "" && !frame.framePrint.over) &&
-                              // {(frame.framePrint.NATALECZKA.text !== "" && !frame.framePrint.over) &&
+                            {(frame.framePrint.text !== "" && !frame.framePrint.over) &&
                               <div style={{ position: "absolute", width: "100%" }}>
                                 <div style={!visual ? { ...autoResizeInputStyle, top: `${frame.framePrint.textY * sc}px`, left: `${frame.framePrint.textX * sc}px`, transition: "0s" } :
                                   { ...autoResizeInputStyle, top: `${frame.framePrint.textY * sc}px`, left: `${frame.framePrint.textX * sc}px`, transition: "0.4s ease" }}>
