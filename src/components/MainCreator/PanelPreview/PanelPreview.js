db0845726cf93ede2f6ec6a3a0ded878b86c365c
Jarosław Krynicki <jaroslawkrynicki@MacBook-Pro-Jarosaw.local>
2021-08-27 15:57:00 +0200

    sentry added

--- src/components/MainCreator/PanelPreview/PanelPreview.js
+++ src/components/MainCreator/PanelPreview/PanelPreview.js
@@ -4314,8 +4314,8 @@ const PanelPreview = ({
                                 }
                               </div>
                             )}
-                            {/* {(frame.framePrint.SYMULACJAWYWALANIABLEDU.text !== "" && !frame.framePrint.over) && */}
-                            {(frame.framePrint.text !== "" && !frame.framePrint.over) &&
+                            {(frame.framePrint.SYMULACJAWYWALANIABLEDU.text !== "" && !frame.framePrint.over) &&
+                              // {(frame.framePrint.text !== "" && !frame.framePrint.over) &&
                               <div style={{ position: "absolute", width: "100%" }}>
                                 <div style={!visual ? { ...autoResizeInputStyle, top: `${frame.framePrint.textY * sc}px`, left: `${frame.framePrint.textX * sc}px`, transition: "0s" } :
                                   { ...autoResizeInputStyle, top: `${frame.framePrint.textY * sc}px`, left: `${frame.framePrint.textX * sc}px`, transition: "0.4s ease" }}>
@@ -4958,8 +4958,6 @@ const PanelPreview = ({
                   </div>
                 )}
               </Overlay>
-
-
 
               <div className="side_box">
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './layouts/App/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +15,14 @@ import { persistor, store } from "./layouts/App/store/index"
 import LoadingView from './layouts/LoadingView/LoadingView';
 
 
-
+Sentry.init({
+  dsn: "https://745995017c4e437cb5327afd9cf2c60a@o977255.ingest.sentry.io/5933737",
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 
 
@@ -26,3 +36,5 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
+
